51697735faf5aae34fb0ad7028bfa742f1c030e9
ipobornik <gereg.sith@gmail.com>
2014-02-20 19:20:37 +0200

    101

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,7 +10,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test 100");
+            Console.WriteLine("test 101");
         }
     }
 }
